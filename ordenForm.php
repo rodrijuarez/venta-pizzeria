fbdf4b3803a9d1b5e34c3145e7b73ea961c46fcb
rodrijuarez <rodrigo.juarez.inf@gmail.com>
2015-09-20 16:02:52 -0300

    Se agrega la vista para poder agregar más de un producto a una misma orden.

--- ordenForm.php
+++ ordenForm.php
@@ -30,6 +30,7 @@ if(isset($_GET["nro_orden"])){
     <script src="js/angular.min.js"></script>
 
     <link href="css/ui-lightness/jquery-ui-1.9.2.custom.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <script src="js/jquery-1.8.3.js"></script>
     <script src="js/jquery-ui-1.9.2.custom.js"></script>
 </head>
@@ -45,21 +46,62 @@ if(isset($_GET["nro_orden"])){
                 echo $nro_orden;
                 ?>">
                 <div class="form-group">
+                    <label>Domicilio del Cliente</label>
                     <input type="text" name="domicilioCliente" class="form-control" placeholder="Domicilio del cliente" required
                     value="<?php
                     echo $orden->domicilio_cliente;
                     ?>">
                 </div>
                 <div class="form-group">
+                    <label>Telefono del Cliente</label>
                     <input type="text" name="telefonoCliente" class="form-control" placeholder="Telefono del cliente" required
                     value="<?php
                     echo $orden->telefono_cliente;
                     ?>">
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading"> Productos Solicitados
+                        <button type="button" class="btn btn-default pull-right add-product" ng-click="addProducto()">+</button>
+                    </div>
+                    <div class="form-group">
+                        <label>Producto</label>
+                        <select type="text" name="productos" class="form-control" ng-options="item.id_producto as item.descripcion for item in productos" ng-model="productoSeleccionado">
+                            <option value="" ng-if="false"></option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>Cantidad</label>
+                        <input type="text" name="Cantidad" class="form-control" placeholder="cantidad">
+                    </div>
                 </div>
                 <input class="btn btn-success pull-right" type="submit" value="Save" />
             </form>
         </div>
     </div>
 </body>
+<script type="text/javascript">
+    var app = angular.module('venta-pizzeria', []);
+
+    app.controller('ordenFormController', function($scope, $http){
+
+        $scope.productos = [];
+
+        $scope.init = function() {
+            $http.get('ajax/traerProductos.php').
+            success(function(response) {
+                $scope.productos = response;
+                $scope.productoSeleccionado = $scope.productos[0];
+            }, function(response) {
+                alert("hola");
+            });
+        };
+        $scope.init();
+
+        $scope.addProducto = function(){
+            alert("Hola");
+        };
+
+    });
+</script>
 </html>
 
