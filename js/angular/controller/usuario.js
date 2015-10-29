00b86623f452d080ed22c06c0820f487663a1784
rodrijuarez <rodrigo.juarez.inf@gmail.com>
2015-10-29 20:32:50 -0300

    Se completa la modificacion y el alta de usuarios. Falta retornar  la clave

--- js/angular/controller/usuario.js
+++ js/angular/controller/usuario.js
@@ -2,7 +2,7 @@ var app = angular.module('venta-pizzeria', []);
 
 app.controller('usuarioFormController', function($scope, $http){
     $scope.guardar = function(usuario){
-        usuario.alta = ($location.search.usuario != undefined) ? false : true;
+        usuario.alta = (getUrlParameter("usuario") != undefined) ? false : true;
         $http.post('/venta-pizzeria/submitUsuario.php',usuario).then($scope.mostrarMensaje);
     }
 
